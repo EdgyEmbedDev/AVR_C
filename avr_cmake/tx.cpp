f5653846557e6837b7d9b1843db44b622b0361dc
EdgyEmbedDev <mikolaj.malecki@thaumatec.com>
2019-07-24 16:04:00 +0200

    Uart time

--- avr_cmake/tx.cpp
+++ avr_cmake/tx.cpp
@@ -1,31 +1,44 @@
 #include <avr/io.h>
-#include <util/delay.h>                
+#include <util/delay.h>
+#include <string.h>
+
+#define OUT_TXD     DDRD |= _BV(1)
+#define IN_RXD      DDRD &= _BV(0) 
+#define UBRRVAL     12
+
+void init_uart(){
+    OUT_TXD;
+    //IN_RXD;
+
+    UBRRH = (unsigned char)(UBRRVAL >> 8);
+    UBRRL = (unsigned char)(UBRRVAL);
+
+    UCSRB |= (1 << RXEN) | (1 << TXEN);
+    UCSRC |= (1 << URSEL) | (1 << UCSZ0)| (1 << UCSZ1);
+}
+
+void send_string(const char* word){
+    int size = strlen(word);
+    for(int i = 0; i < size ; i++){
+        while(!(UCSRA & (1 << UDRE)));
+        UDR = word[i];
+    }
+}
+
 int main(void)
 {
-    // DDRB  |= _BV(0)|_BV(1);
-    // PORTB |= _BV(0);
-    // PORTB &= ~_BV(1);
-    // DDRD  &= ~_BV(0);
-    // PORTD |=  _BV(0);
+    int i = 0;
+    init_uart();
 
-    // while (1) 
-    // {
-    //     PORTB ^=_BV(0);
-    //     PORTB ^=_BV(1);
-        
-    //     _delay_ms(200);
-    // }
-    DDRD |= 0xff;
-    PORTD |= 0x01;
-    bool flag = true;
+    DDRB |= _BV(0);
+    PORTB |= _BV(0);
     while (1)
     {
-        
-        for(int i = 0 ; i < 7 ; i++){
-            PORTD = flag ?  PORTD << 1 : PORTD >> 1; 
-        }
-        PORTD = PORTD >>1;
+        // PORTB ^= _BV(0);
+        // while(!(UCSRA & (1 << UDRE))){};
+        // UDR = i++ % 2 ?  'A' : 'B';
+        send_string("siema\r\n");
         _delay_ms(200);
-    }
+    };
     
 }
